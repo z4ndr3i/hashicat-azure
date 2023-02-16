2449e6e5ed63d5bddebe1889975911a3c3092f7b
Andrei Z <z.andrei@gmail.com>
2023-02-16 20:08:41 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,11 +1,6 @@
-resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "Canada Central"
-}
-
 module "network" {
   source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -21,5 +16,5 @@ module "network" {
     costcenter  = "it"
   }
 
-  depends_on = [azurerm_resource_group.example]
+  depends_on = [azurerm_resource_group.myresourcegroup]
 }
